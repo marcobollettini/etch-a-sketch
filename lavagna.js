32dc8ce63bb43131a768ad7c7cadb4d5e5fe1c2c
marcobollettini <55404322+marcobollettini@users.noreply.github.com>
2019-12-13 11:23:34 +0100

    funzione pulizia e nuova lavagna

--- lavagna.js
+++ lavagna.js
@@ -1,6 +1,17 @@
+//Variabili 
 let lato = 32;
 const lavagna = document.querySelector(".container"); 
+const btn = document.querySelector("button");
 
+//Pulizia e Creazione nuova lavagna col bottone
+
+btn.addEventListener("click", () =>{
+    lavagna.innerHTML = "";
+    lato = prompt("indica il lato della nuova lavagna", "32");
+    creaLavagna(lato);
+})
+
+//Funzione per la creazione della lavagna
 const creaLavagna = (asse) => {
     for(x = 0; x < asse; x++){
         for(y = 0; y < asse; y++){
@@ -17,6 +28,8 @@ const creaLavagna = (asse) => {
 
 creaLavagna(lato);
 
+//Funzione per colorare la lavagna tramite modifica della classe
 lavagna.addEventListener('mouseover', e => {
-    e.target.style.backgroundColor = "black";
+    e.target.classList.add('pass');
 })
+
